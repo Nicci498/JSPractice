e3102d74e56f96b346e02b02e3adb80572398b81
Nicole White <nicci498@gmail.com>
2020-12-07 10:19:35 -1000

    func with if

--- ifsandcompares.js
+++ ifsandcompares.js
@@ -13,6 +13,14 @@ const id = 100
      console.log('incorrect')
  }
 
+ function isEven(id){
+     if(id % 2 !== 0){
+         return 'Not even'
+     } else{
+         return 'Even'
+     }
+ }
+
  // NOT equal to
  if(id !== 100){
     console.log('correct')
