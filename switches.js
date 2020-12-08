dad7ce05fedfff5a4c72cc9fcc4b677cdc7300bf
Nicole White <nicci498@gmail.com>
2020-12-08 07:49:50 -1000

    switch

--- switches.js
+++ switches.js
@@ -41,3 +41,25 @@ switch(new Date().getDay()){
         day = 'so confused, where am i?'
         break;
 }
+
+switch(stash){
+    case stash > 100:
+        console.log('plenty');
+        break;
+    case stash > 75:
+        console.log('we are good for now');
+        break;
+    case stash > 50:
+        console.log('keep an eye on it');
+        break;
+    case stash > 15:
+        console.log('order more');
+        break;
+    case stash === 0:
+        console.log('oh no we are all of of things');
+        break;
+    default:
+        console.log('what is thins?');
+        break;
+
+}
