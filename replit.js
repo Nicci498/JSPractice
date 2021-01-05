2ad7bb7092f8c05222131bb7d472e620f93d42b8
Nicole White <nicci498@gmail.com>
2021-01-04 18:55:03 -1000

    longest string problem

---
+++ replit.js
@@ -0,0 +1,14 @@
+// longest string
+let arr = strings1
+function longestString(arr) {
+  let longest = "";
+    for(i = 0; i < arr.length; i++){
+      if(arr[i].length > longest.length){
+        longest = arr[i]
+      }
+    }
+    return longest
+}
+
+const strings1 = ['short', 'really, really long!', 'medium'];
+console.log(longestString(strings1)); // <--- 'really, really long!'
