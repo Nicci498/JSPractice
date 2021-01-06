7c2a4216701324d67bc2228160d3d908f0aa2ef6
Nicole White <nicci498@gmail.com>
2021-01-05 14:13:05 -1000

    reverse with loop

--- replit.js
+++ replit.js
@@ -14,9 +14,16 @@ const strings1 = ['short', 'really, really long!', 'medium'];
 console.log(longestString(strings1)); // <--- 'really, really long!'
 
 //reversed string
-function reverseStringMethods(str) {
+function reverseStringFunctions(str) {
     return str.split("").reverse().join("");
   }
-  
   reverseString("LLO")
 
+function reverseString(str) {
+    let reversedString = "";
+    for(let i = str.length - 1; i >= 0; i--){
+      reversedString += str[i];
+    }
+    return reversedString
+}
+  reverseString("LLO")
